82abd5f30d9e7d539b304b72806d8b7539c142e6
rjg26247 <rjg26247@gmail.com>
2015-04-01 16:37:37 +0000

    try to add back deleted JUnit tests in db and domin

--- menusearch/test/menusearch/db/MenuDBAccessTest.java
+++ menusearch/test/menusearch/db/MenuDBAccessTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package menusearch.db;
 
 import menusearch.domain.Menu;
@@ -12,76 +7,273 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import menusearch.domain.Dish;
 
 /**
- *
+ * Tests the MenuDBAccess class with JUnit.
+ * 
+ * @author Randy Gingeleski
  * @author nicolebkim
  */
 public class MenuDBAccessTest {
     
-    public MenuDBAccessTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
+    public MenuDBAccessTest() { }
     
     @Before
     public void setUp() {
-    }
     
-    @After
-    public void tearDown() {
+        try {
+            DBConnection.init();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(MenuDBAccessTest.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        } 
     }
 
-    /**
-     * Test of retrieve method, of class MenuDBAccess.
-     */
     @Test
-    public void testRetrieve() throws Exception {
+    public void testRetrieve_noResult() throws Exception {
         System.out.println("retrieve");
         String query = "";
         Menu expResult = null;
-  //      Menu result = MenuDBAccess.retrieve(query);
-   //     assertEquals(expResult, result);
-    }
 
-    /**
-     * Test of retrieveByID method, of class MenuDBAccess.
-     */
+   //   Menu result = MenuDBAccess.retrieve(query).get(0);
+   //   assertEquals(expResult, result);
+    }
+    
     @Test
-    public void testRetrieveByID() throws Exception {
+    public void testRetrieveByMenuID_getID() {
+        
+        Menu testMenu = null;
+        
+        try {
+            testMenu = MenuDBAccess.retrieveByMenuID("12463");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(MenuDBAccessTest.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(MenuDBAccessTest.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        }
+
+        assertEquals(12463, testMenu.getMenu_id());
+    }
+    
+    @Test
+    public void testRetrieveByMenuID_getSponsor() {
+        Menu testMenu = null;
+        
+        try {
+            testMenu = MenuDBAccess.retrieveByMenuID("12463");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(MenuDBAccessTest.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(MenuDBAccessTest.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        }
+
+        assertEquals("HOTEL EASTMAN", testMenu.getSponsor());
+    }
+    
+    @Test
+    public void testRetrieveByMenuID_getEvent() {
+                
+        Menu testMenu = null;
+        
+        try {
+            testMenu = MenuDBAccess.retrieveByMenuID("12463");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(MenuDBAccessTest.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(MenuDBAccessTest.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        }
+
+        assertEquals("BREAKFAST", testMenu.getEvent());
+    }
+  
+    @Test
+    public void testRetrieveByMenuID_noInput() throws Exception {
         System.out.println("retrieveByID");
         String menu_id = "";
         Menu expResult = null;
-        Menu result = MenuDBAccess.retrieveByID(menu_id);
+        Menu result = MenuDBAccess.retrieveByMenuID(menu_id);
         assertEquals(expResult, result);
     }
-
-    /**
-     * Test of populateMenupages method, of class MenuDBAccess.
-     */
+    
     @Test
-    public void testPopulateMenupages() throws Exception {
-        System.out.println("populateMenupages");
-        Menu menu = null;
-        MenuDBAccess.populateMenupages(menu);
-    }
-
-    /**
-     * Test of retrieveByMenuID method, of class MenuDBAccess.
-     */
-    @Test
-    public void testRetrieveByMenuID() throws Exception {
+    public void testRetrieveByMenuID_noResult() throws Exception {
         System.out.println("retrieveByMenuID");
-        int id = 0;
+        String id = "0";
         Menu expResult = null;
         Menu result = MenuDBAccess.retrieveByMenuID(id);
         assertEquals(expResult, result);
     }
     
+    @Test
+    public void testSearchByVenue() throws Exception {
+        ArrayList<Menu> testMenus = null;
+        
+        try {
+            testMenus = MenuDBAccess.searchByVenue("Hotel Eastman");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(MenuDBAccessTest.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(MenuDBAccessTest.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        }
+        
+        assertEquals(5, testMenus.size());
+        // Hard-coded to expected SQL results
+    }
+    
+    @Test
+    public void testSearchByPlace() throws Exception {
+        ArrayList<Menu> testMenus = null;
+        
+        try {
+            testMenus = MenuDBAccess.searchByPlace("En Route");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(MenuDBAccessTest.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            Logger.getLogger(MenuDBAccessTest.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        }
+        
+        assertEquals(293, testMenus.size());
+        // Hard-coded to expected SQL results
+    }
+    
+    @Test
+    public void testSearchByDish() throws Exception {
+
+        Dish thisDish = new Dish(12678);
+        ArrayList<Menu> result = MenuDBAccess.searchByDish(thisDish);
+        
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    public void testGetDishIDsFromName() throws Exception {
+
+        String dishName = "pineapple";
+        ArrayList<Integer> result = MenuDBAccess.getDishIDsFromName(dishName);
+        
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    public void testSearchByYear_Exact() throws Exception {
+
+        int[] yearParams = {1972, 1972};
+        ArrayList<Menu> result = MenuDBAccess.searchByYear(yearParams);
+        
+        assertEquals(30, result.size());
+    }
+
+    @Test
+    public void testSearchByCurrency() throws Exception {
+
+        String currency = "Francs";
+        ArrayList<Menu> result = MenuDBAccess.searchByCurrency(currency);
+        
+        assertEquals(162, result.size());
+    }
+
+    @Test
+    public void testSearchByEvent() throws Exception {
+
+        String event = "Breakfast";
+        ArrayList<Menu> result = MenuDBAccess.searchByEvent(event);
+        
+        assertEquals(926, result.size());
+    }
+
+    @Test
+    public void testSearchBySponsor() throws Exception {
+
+        String sponsor = "Republican House";
+        ArrayList<Menu> result = MenuDBAccess.searchBySponsor(sponsor);
+        
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    public void testSearchByOccasion() throws Exception {
+
+        String occasion = "Daily";
+        ArrayList<Menu> result = MenuDBAccess.searchByOccasion(occasion);
+        
+        assertEquals(232, result.size());
+    }
+
+    @Test
+    public void testSearchByPageCount_Exact() throws Exception {
+
+        int[] pageCountParams = {31, 31};
+        ArrayList<Menu> result = 
+                MenuDBAccess.searchByPageCount(pageCountParams);
+        
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    public void testSearchByDishCount_Exact() throws Exception {
+
+        int[] dishCountParams = {72, 72};
+        ArrayList<Menu> result = 
+                MenuDBAccess.searchByDishCount(dishCountParams);
+        
+        assertEquals(89, result.size());
+    }
+
+    @Test
+    public void testRetrieveByComplexSearch_justGeneral() throws Exception {
+        
+        // General query
+        SearchParameters params = new SearchParameters("New York");
+        
+        ArrayList<Menu> result = MenuDBAccess.retrieveByComplexSearch(params);
+        
+        assertEquals(70, result.size()); // Expecting 70 results
+    }
+    
+    @Test
+    public void testRetrieveByComplexSearch_generalWithYear() throws Exception {
+        
+        // General query
+        SearchParameters params = new SearchParameters("New York");
+        
+        int[] yearArray = {1900, 1900}; // Filter to year 1900
+        params.setYear(yearArray);
+        
+        ArrayList<Menu> result = MenuDBAccess.retrieveByComplexSearch(params);
+        
+        assertEquals(19, result.size()); // Expecting 19 results
+    }
+
+    @Test
+    public void testPopulateMenupages() throws Exception {
+        System.out.println("populateMenupages");
+        Menu menu = null;
+        MenuDBAccess.populateMenupages(menu);
+    }    
 }
